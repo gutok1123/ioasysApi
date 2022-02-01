d0c9148968ed840b7bfb9e172ae374cac412e91e
gutok1123 <gutok11@hotmail.com>
2022-02-01 18:19:37 -0300

    d

--- src/domain/repository/user.js
+++ src/domain/repository/user.js
@@ -1,13 +1,21 @@
+const { Email } = require("@material-ui/icons");
 const connection = require("../../infra/database/connection");
 
-const getUserById = async (id) => {
-  const results = await connection.query(
-    `SELECT email FROM users WHERE id = ${id}`
-  );
+const getById = async (id) => {
+  const results = await Users.findOne({
+                            attributes:['email'],
+                              where: {
+                                id: `${id}`
+                              }
+             });
+ 
+  //   `SELECT email FROM users WHERE id = ${id}`
+  // );
 
+  
   return results.rows;
 };
 
 module.exports = {
-  getUserById,
+  getById,
 };
